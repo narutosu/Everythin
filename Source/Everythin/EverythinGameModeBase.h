c642f07b02466668f5e1b7716594d3486b7c81db
H61IB5G6O7GUYYB\Administrator <1207173433@qq.com>
2021-12-10 15:21:27 +0800

    init

---
+++ Source/Everythin/EverythinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EverythinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVERYTHIN_API AEverythinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
